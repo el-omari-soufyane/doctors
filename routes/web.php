fc030e8d739724b91fbb3ba54e367cd13b8bb620
ELMAALM <tcsbilal@gmail.com>
2020-05-22 15:48:09 +0000

    last commit

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,24 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+Route::get("/",function()
+{     if(Auth::check())
+     {
+        return redirect("/home");
+     }
+     else
+     {
+        return view('auth.login');
+     }
+});
 
+Route::group(['middleware'=>'auth'],function()
+{
 Route::get('/home', 'HomeController@home')->name('home');
 Route::get('/patients/getpatient', 'PatientController@getpatient')->name('patients.getpatient');
 Route::resource('/patients', 'PatientController');
 Route::resource('/appointment', 'AppointmentController');
 Route::resource('/consultation', 'ConsultationController');
-
+});
 
